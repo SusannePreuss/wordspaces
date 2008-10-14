1efa1fc2ec49e83c53075ad872148a89da595ae4
alexanderfrey <alexanderfrey@179fc948-6854-0410-ab3f-75f2cdb202a9>
2008-10-14 09:17:01 +0000

    New procedure to calculate group grade --> Testing !!! Clean ups.

--- WordSpaces/src/wordspaces/GroupGradeCalculator.java
+++ WordSpaces/src/wordspaces/GroupGradeCalculator.java
@@ -25,24 +25,26 @@ public class GroupGradeCalculator {
          * result[2] = possible_max_error 
          */
         double[] result = new double[3];
-        float grade = 0, error = 0, group_error = 0, possible_maximal_error = 0;
         Iterator<Integer> groupIter = groups.keySet().iterator();
         Iterator<Entry<String,Double>> k_smallest_edges_iter;
         String word, neighborWord;
         Vector<String> members;
         TreeSet<Entry<String, Double>> k_smallest_edges;
         Entry entry;
+        float grade = 0, 
+              error = 0, 
+              group_error = 0, 
+              possible_maximal_error = 0;
+        
         int grp, grp_SIZE, errors = 0,
-            word_counter = 0, 
             pos_in_k_smallest_edges = 0;     //pos of the word of k_smallest_edges, needed to calculate the error
 
         while(groupIter.hasNext()){          //go through all groups
-            System.out.println("!!!!!!NEW GROUP!!!!");
+            System.out.println("!!!! NEW GROUP !!!!");
             grp = groupIter.next();
             members = groups.get(grp);
             //first identify the number of words in the same group as vectorName
             grp_SIZE = members.size();
-            word_counter += (members.size()-1) * members.size();
             group_error = 0;
             possible_maximal_error += calcMaxGroupError(grp_SIZE);
             
@@ -60,19 +62,20 @@ public class GroupGradeCalculator {
                     pos_in_k_smallest_edges++;                  //first word is at pos 1 and so on...
                     /* now check if this neighborWord is in the same group as word, if so give points. */
                     if(!members.contains(neighborWord)){
-                        /* Calculate the error by dividing 1 by pos_in_k_smallest_edges and add it to grade */
+                        /* Calculate the error by dividing 1 by pos_in_k_smallest_edges */
                         error = (float) 1 / (float) pos_in_k_smallest_edges;
-                        group_error += error;
+                        group_error += error / possible_maximal_error;
                         errors++;
                         System.out.println("Error! "+word.toUpperCase()+" neighbor is "+neighborWord.toUpperCase()+" pos is "+pos_in_k_smallest_edges+" error "+error);
-                     }
+                    }
                 }
             }
-            /* Normalize the group_error by dividing it by the size of the group */
+            /* Calculate the arithmetic average of the grp_error by dividing grp_error
+             * by grp_SIZE. We get the average error for a word in the group. */
             group_error = group_error / (float) grp_SIZE;
             grade += group_error;
         }
-        result[0] = (possible_maximal_error-grade) / possible_maximal_error;
+        result[0] = 1 - ( grade / groups.size() );
         result[1] = errors;
         result[2] = possible_maximal_error;
         return result;
---
+++ WordSpaces/src/wordspaces/gui/threads/CalculateDistance.java
@@ -0,0 +1,110 @@
+/*
+ * CalculateDistance.java
+ *
+ * Created on 14. Juni 2007, 11:16
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package wordspaces.gui.threads;
+
+import javax.swing.SwingWorker;
+import plugins.ComputesDistance;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+import exceptions.DimensionNotEqualException;
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import plugins.EuclideanDistance;
+import wordspaces.Fust;
+
+/**
+ *
+ * @author alexander
+ */
+public class CalculateDistance extends SwingWorker<Object,String>{
+    
+    private TreeMap<String,SortedMap> vectors;
+    private Map cachedDistances;
+    private ComputesDistance cDist;
+
+    
+    public CalculateDistance(TreeMap<String,SortedMap> vectors, Map distances, ComputesDistance cDist){
+        this.cDist           = cDist;
+        this.vectors         = vectors;
+        this.cachedDistances = distances; 
+    }
+    
+    
+    protected Object doInBackground() throws Exception {
+        try {
+            getDistances();
+        } catch (DimensionNotEqualException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+    
+    public void getDistances() throws DimensionNotEqualException{
+        Entry<String, SortedMap> firstEntry  = null;
+        Iterator iter            = null;
+        String word              = null;
+        String compareWord       = null;
+        SortedMap vector         = null;
+        SortedMap compareVector  = null;
+        double distance;
+        
+        while(!vectors.isEmpty()){
+            /* Get and delete the first entry from vectors */
+            firstEntry = vectors.pollFirstEntry();
+            word       = (String)firstEntry.getKey();
+            vector     = (SortedMap)firstEntry.getValue();
+            iter       = vectors.keySet().iterator();
+            /* Run through the rest of the entries from vector */
+            while(iter.hasNext()){
+                compareWord   = (String)iter.next();
+                compareVector = vectors.get(compareWord);
+                //check whether the distance between the two words has already been computed
+                if(!cachedDistances.containsKey(word) || !((SortedMap)cachedDistances.get(word)).containsKey(compareWord)  ){
+                    Fust.mergeVectors(vector,compareVector);
+                    distance = sum(cDist.compute(vector,compareVector));
+
+                    if(cDist instanceof EuclideanDistance)
+                        distance = 1 / Math.sqrt(distance);
+                    
+                    //now save the results to cachedDistances
+                    if(cachedDistances.containsKey(word)){
+                        ((TreeMap)cachedDistances.get(word)).put(compareWord,distance);                       
+                    } else {
+                        cachedDistances.put(word, new TreeMap<String, Double>());
+                        ((TreeMap)cachedDistances.get(word)).put(compareWord,distance);
+                    }
+                    if(cachedDistances.containsKey(compareWord)){
+                        ((TreeMap)cachedDistances.get(compareWord)).put(word,distance);   
+                    } else {
+                         cachedDistances.put(compareWord, new TreeMap<String, Double>());
+                        ((TreeMap)cachedDistances.get(compareWord)).put(word,distance);
+                    }
+                }               
+            }
+            this.firePropertyChange("progress",null,word);
+        }
+    } 
+      
+    public static double getDistance(SortedMap vector1, SortedMap vector2, ComputesDistance cDist) throws DimensionNotEqualException{        
+        Fust.mergeVectors(vector1, vector2);
+        return sum(cDist.compute(vector1, vector2));
+    } 
+    
+    public static double sum(double[] vector){
+        double result = 0;
+        for(int i=0; i<vector.length;i++){
+            result += vector[i];
+        }
+        
+        return result;
+    }
+
+}
